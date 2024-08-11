c615a5bd7a0655abc559de1d4b2bbe5616d1d184
HabibAdavize <131537314+HabibAdavize@users.noreply.github.com>
2024-08-11 11:19:53 +0100

    Got the ETH transaction working

--- deploy.js
+++ deploy.js
@@ -3,17 +3,22 @@ const fs = require("fs-extra");
 
 async function main() {
   // Set up the provider with the correct URL
-  const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
 
   // Initialize the wallet with a private key and the provider
   const wallet = new ethers.Wallet(
-    "0xe3e4bceddd36c817dff2e7a6027dfcef6e6a31e2bd19b82aa8ee93f97e05ebdf",
+    "0x56b8ae52a7aabed6838411e5b83f658c179313fc240fee048409f2614a87513f",
     provider
   );
 
   // Read the ABI and binary code from the file system
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-  const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
 
   // Create a ContractFactory instance
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
@@ -21,8 +26,7 @@ async function main() {
 
   // Deploy the contract
   const contractInstance = await contractFactory.deploy();
-  await contractInstance.deployed(); // Wait for the deployment to be mined
-  console.log("Contract deployed to:", contractInstance.address);
+  console.log("Contract deployed to:", contractInstance);
 }
 
 main()
